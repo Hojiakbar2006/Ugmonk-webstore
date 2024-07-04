7453e1e920d6ac1e003919321a4853bef13d348b
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-07-04 23:01:25 +0500

    fix

--- ugmonk-backend/products/admin.py
+++ ugmonk-backend/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category, View, Order
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(View)
+admin.site.register(Order)
---
+++ ugmonk-frontend/calculator/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <form>
+        <input />
+      </form>
+    </div>
+  );
+}
+
+export default App;
